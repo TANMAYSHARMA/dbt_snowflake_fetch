2ad093818344cf09bb9e50285d781727de929c73
TANMAY SHARMA <tanmay379@gmail.com>
2024-06-05 07:42:41 -0400

    Update brands_interim.sql

--- models/intermediary_staging/brands_interim.sql
+++ models/intermediary_staging/brands_interim.sql
@@ -12,16 +12,16 @@ with brands_deduped as (
     select
         brand_id,
         barcode,
-		brand_code,
-		brand_name,
-		is_top_brand,
+	brand_code,
+	brand_name,
+	is_top_brand,
         category,
         category_code,
         cpg_id,
         cpg_ref
     from
         {{ ref('brands_flattened') }} as bs,
-	{{ dbt_utils.group_by(9) }}
+    {{ dbt_utils.group_by(9) }}
 
 )
 
