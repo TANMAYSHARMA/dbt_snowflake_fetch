02dfabe395eca05aecf909ff2df1f6dd629f2b8f
TANMAY SHARMA <tanmay379@gmail.com>
2024-06-05 07:39:00 -0400

    Update users_flattened.sql

--- models/sources/users_flattened.sql
+++ models/sources/users_flattened.sql
@@ -12,9 +12,9 @@ with users_flattened as (
     select
         s.raw_users_json:_id:"$oid"::varchar as user_id,
         s.raw_users_json:state::varchar as state,
-		s.raw_users_json:createdDate:"$date"::varchar::timestamp as created_date,
-		s.raw_users_json:lastLogin:"$date"::varchar::timestamp as last_login_date,
-		s.raw_users_json:role::varchar as role,
+	s.raw_users_json:createdDate:"$date"::varchar::timestamp as created_date,
+	s.raw_users_json:lastLogin:"$date"::varchar::timestamp as last_login_date,
+	s.raw_users_json:role::varchar as role,
         s.raw_users_json:active::boolean as is_active,
         s.raw_users_json:signUpSource::varchar as sign_up_source
     from
