99f9f547e798b486cf1d13b3508e2f384c4fc56a
TANMAY SHARMA <tanmay379@gmail.com>
2024-06-05 07:39:18 -0400

    Update brands_flattened.sql

--- models/sources/brands_flattened.sql
+++ models/sources/brands_flattened.sql
@@ -12,9 +12,9 @@ with brands_flattened as (
     select
         s.raw_brand_json:_id:"$oid"::varchar as brand_id,
         s.raw_brand_json:barcode::varchar as barcode,
-		s.raw_brand_json:brandCode::varchar as brand_code,
-		s.raw_brand_json:name::varchar as brand_name,
-		s.raw_brand_json:topBrand::boolean as is_top_brand,
+	s.raw_brand_json:brandCode::varchar as brand_code,
+	s.raw_brand_json:name::varchar as brand_name,
+	s.raw_brand_json:topBrand::boolean as is_top_brand,
         s.raw_brand_json:category::varchar as category,
         s.raw_brand_json:categoryCode::varchar as category_code,
         s.raw_brand_json:cpg:"$id":"$oid"::varchar as cpg_id,
