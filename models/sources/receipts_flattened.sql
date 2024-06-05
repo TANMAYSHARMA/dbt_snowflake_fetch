0ab251956dc6bd64ed4b41b0d7c0edb4610b7487
TANMAY SHARMA <tanmay379@gmail.com>
2024-06-05 07:39:45 -0400

    Update receipts_flattened.sql

--- models/sources/receipts_flattened.sql
+++ models/sources/receipts_flattened.sql
@@ -12,9 +12,9 @@ with receipts_flattened as (
     select
         s.raw_receipts_json:_id:"$oid"::varchar as receipt_id,
         s.raw_receipts_json:bonusPointsEarned::int as bonus_points_earned,
-		s.raw_receipts_json:bonusPointsEarnedReason::varchar as bonus_points_earned_reason,
-		s.raw_receipts_json:createDate:"$date"::varchar::timestamp as created_date,
-		s.raw_receipts_json:dateScanned:"$date"::varchar::timestamp as scanned_date,
+	s.raw_receipts_json:bonusPointsEarnedReason::varchar as bonus_points_earned_reason,
+	s.raw_receipts_json:createDate:"$date"::varchar::timestamp as created_date,
+	s.raw_receipts_json:dateScanned:"$date"::varchar::timestamp as scanned_date,
         s.raw_receipts_json:finishedDate:"$date"::varchar::timestamp as finished_date,
         s.raw_receipts_json:modifyDate:"$date"::varchar::timestamp as modify_date,	
         s.raw_receipts_json:pointsAwardedDate:"$date"::varchar::timestamp as points_awarded_date,
